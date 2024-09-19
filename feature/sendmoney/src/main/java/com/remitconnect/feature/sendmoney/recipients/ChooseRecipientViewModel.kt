b49bbd4e25f75024f17c0c4a661ca196eb2a94e1
yveskalume <yvkalume@gmail.com>
2024-09-19 23:47:43 +0200

    remove unnecessary imports

--- feature/sendmoney/src/main/java/com/remitconnect/feature/sendmoney/recipients/ChooseRecipientViewModel.kt
+++ feature/sendmoney/src/main/java/com/remitconnect/feature/sendmoney/recipients/ChooseRecipientViewModel.kt
@@ -5,12 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.remitconnect.domain.model.Recipient
 import com.remitconnect.domain.repository.RecipientRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
@@ -24,19 +22,19 @@ internal class ChooseRecipientViewModel @Inject constructor(
     val searchText: StateFlow<String> = _searchText.asStateFlow()
 
     val uiState: StateFlow<ChooseRecipientUiState> = searchText.map { text ->
-            recipientRepository.getRecipients().fold(
-                onSuccess = { recipients ->
-                    recipients.filterRecipientsSearchText(text)
-                },
-                onFailure = {
-                    ChooseRecipientUiState.Error(it.message ?: "Unknown error")
-                }
-            )
-        }.stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5_000),
-            initialValue = ChooseRecipientUiState.Loading
+        recipientRepository.getRecipients().fold(
+            onSuccess = { recipients ->
+                recipients.filterRecipientsSearchText(text)
+            },
+            onFailure = {
+                ChooseRecipientUiState.Error(it.message ?: "Unknown error")
+            }
         )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5_000),
+        initialValue = ChooseRecipientUiState.Loading
+    )
 
 
     fun handleAction(action: ChooseRecipientAction) {
