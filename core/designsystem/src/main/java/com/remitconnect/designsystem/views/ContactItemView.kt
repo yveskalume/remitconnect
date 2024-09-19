3579522e79a78de26ba0a27caf5d0c6f95894dcf
yveskalume <yvkalume@gmail.com>
2024-09-19 21:03:49 +0200

    update contact image placeholder

--- core/designsystem/src/main/java/com/remitconnect/designsystem/views/ContactItemView.kt
+++ core/designsystem/src/main/java/com/remitconnect/designsystem/views/ContactItemView.kt
@@ -1,6 +1,8 @@
 package com.remitconnect.designsystem.views
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -11,11 +13,12 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.remitconnect.designsystem.theme.RemitConnectTheme
@@ -38,14 +41,19 @@ fun ContactItemView(
             .padding(vertical = 18.5.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Surface(
+        Box(
             modifier = Modifier
                 .padding(end = 8.dp)
-                .size(40.dp),
-            shape = MaterialTheme.shapes.small,
-            color = MaterialTheme.colorScheme.surfaceGray
+                .size(40.dp)
+                .clip(MaterialTheme.shapes.small)
+                .background(MaterialTheme.colorScheme.surfaceGray),
+            contentAlignment = Alignment.Center
         ) {
-
+            Text(
+                text = rememberUserProfileFromName(name),
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.primary
+            )
         }
         Column {
             Text(text = name, style = MaterialTheme.typography.bodyMedium)
@@ -61,6 +69,14 @@ fun ContactItemView(
             contentDescription = null
         )
     }
+}
+
+@Composable
+private fun rememberUserProfileFromName(name: String) = remember(name) {
+    val splitName = name.split(" ")
+    val firstName = splitName.first().takeIf { it.isNotBlank() }?.take(1) ?: ""
+    val lastName = splitName.last().takeIf { it.isNotBlank() }?.take(1) ?: ""
+    return@remember "$firstName$lastName"
 }
 
 @Preview(showBackground = true)
